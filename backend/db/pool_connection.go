80c44aaec4b16281af6d8b0238019570ddf6d317
Van Phan Mac Air <pvphat25@gmail.com>
2024-09-18 10:43:55 -0400

    add connection, error for path

---
+++ backend/db/pool_connection.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var (
+	DBPool *pgxpool.Pool
+)
+
+func InitPostgresPoolConnection() {
+	var err error
+	DBPool, err = pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool to Postgresql: %v\n", err)
+		os.Exit(1)
+	}
+	err = DBPool.Ping(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to ping connection pool to Postgresql: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stderr, "Connect to Postgresql successfully!\n\n")
+	// Defer to close the db pool later
+	defer DBPool.Close()
+}
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import {
+	"fmt"
 
+}
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("App Started")
+
+	db.InitPostgresPoolConnection()
+	
 }
