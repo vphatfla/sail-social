ed3076a5ed03a2e0a26bfbf04770967cc4875612
van-xps-ubuntu <pvphat25@gmail.com>
2024-09-21 23:07:54 -0400

    add gitignore

--- backend/db/creatorQuery/signUpQuery.go
+++ backend/db/creatorQuery/signUpQuery.go
@@ -8,18 +8,18 @@ import (
 
 func CheckIfEmailExists(email string) (bool, error) {
 	var check bool
-	err := db.DBPool.QueryRow(context.Background(), "SELECT EXISTS(SELECT 1 FROM creator_credential WHERE email = $1)", email).Scan(&check)
+	err := db.DBPool.QueryRow(context.Background(), "SELECT EXISTS(SELECT 1 FROM creator_credential WHERE email = $1);", email).Scan(&check)
 	return check, err
 }
 
 func CheckIfPhoneNumberExists(phoneNumber string) (bool, error) {
 	var check bool
-	err := db.DBPool.QueryRow(context.Background(), "SELECT EXISTS(SELECT 1 FROM creator_credential WHERE phone_number = $1)", phoneNumber).Scan(&check)
+	err := db.DBPool.QueryRow(context.Background(), "SELECT EXISTS(SELECT 1 FROM creator_credential WHERE phone_number = $1);", phoneNumber).Scan(&check)
 	return check, err
 }
 
 func InsertNewRecord(creator model.CreatorCredential) error {
-	_, err := db.DBPool.Exec(context.Background(), "INSERT INTO creator_credential (email, password, phone_number, is_verified) VALUES ($1, $2, $3, $4)",
+	_, err := db.DBPool.Exec(context.Background(), "INSERT INTO creator_credential (email, password, phone_number, is_verified) VALUES ($1, $2, $3, $4);",
 		creator.Email, creator.HashedPassword, creator.PhoneNumber, false)
 	return err
 }
