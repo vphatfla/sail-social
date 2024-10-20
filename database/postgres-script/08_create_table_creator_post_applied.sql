8f159cdd55f766217f6bbb32421299dd3b1bd04a
van-xps-ubuntu <pvphat25@gmail.com>
2024-10-20 15:24:13 -0400

    add message field for applied

--- database/postgres-script/08_create_table_creator_post_applied.sql
+++ database/postgres-script/08_create_table_creator_post_applied.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS "creator_post_applied";
 
 CREATE TABLE "creator_post_applied" (
-    creator_id INT REFERENCES "creator_info"(id),  -- Foreign key reference to creator_info(id)
-    post_id INT REFERENCES "post"(id),                    -- Foreign key reference to post(id)
-    PRIMARY KEY (creator_id, post_id)                  -- Composite unique primary key
+    creator_id INT REFERENCES "creator_info"(id),  
+    post_id INT REFERENCES "post"(id),
+    message VARCHAR(255),
+    PRIMARY KEY (creator_id, post_id)
 );
