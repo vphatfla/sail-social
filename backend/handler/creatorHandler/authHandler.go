4a3abbffc1c65e6b4fbac17f4ceb96ed44f4286a
Van Phan Mac Air <pvphat25@gmail.com>
2024-11-29 23:21:02 -0500

    refactor the file

--- backend/handler/creatorHandler/authHandler.go
+++ backend/handler/creatorHandler/authHandler.go
@@ -10,6 +10,52 @@ import (
 	"vphatlfa/booster-hub/model"
 )
 
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	var creatorCredential model.CreatorCredential
+	// Decode the json payload
+	err := json.NewDecoder(r.Body).Decode(&creatorCredential)
+
+	if err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(customError.ErrorMessage{Message: err.Error() + " json payload invalid"})
+		return
+	}
+
+	hashedPassword, err := creatorQuery.GetHashPassword(creatorCredential.Email)
+
+	if err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(customError.ErrorMessage{Message: err.Error()})
+		return
+	}
+
+	err = hashing.VerifyPassword(hashedPassword, creatorCredential.RawPassword)
+
+	if err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(customError.ErrorMessage{Message: err.Error() + " passwords do not match"})
+		return
+	}
+
+	creatorCredential, err = creatorQuery.GetCreatorCredentialByEmail(creatorCredential.Email)
+	if err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(customError.ErrorMessage{Message: err.Error() + " passwords do not match"})
+		return
+	}
+
+	// token
+	token, err := jwtToken.GenerateJWTToken(creatorCredential.ID, "creator", creatorCredential.IsOnboarded)
+	if err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(customError.ErrorMessage{Message: "Token : " + err.Error()})
+		return
+	}
+
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
+}
+
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	var creatorCredential model.CreatorCredential
 
